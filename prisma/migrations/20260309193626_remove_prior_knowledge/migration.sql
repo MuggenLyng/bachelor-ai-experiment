fade6b7ddeff8f1e656b34abc57e2ac8ec53fdc4
mahatmagandi <lyngmagnus@gmail.com>
2026-03-09 20:36:56 +0100

    fjernet prior_knowledge

---
+++ prisma/migrations/20260309193626_remove_prior_knowledge/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `priorKnowledge` on the `ParticipantLog` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ParticipantLog" DROP COLUMN "priorKnowledge";
